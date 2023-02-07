0d1813e614f77a94609adf6a7fca9ba86fac8be3
marcosrios1 <PhillyRios@gmail.com>
2023-02-07 01:57:09 -0600

    Fixed error that caused none data type when returning value in functions

--- population.py
+++ population.py
@@ -3,6 +3,7 @@ born = 0
 passed = 0
 ending = 0
 
+
 def findpop():
     global n, born, passed, ending
     years = 0
@@ -19,26 +20,33 @@ def findpop():
         years += 1
         
     print(f'Years: {years}')
-    print(n)
+
 
 def starting():
-    n_size = int(input('Starting population: '))
-    if n_size < 9:
-        print('Starting pop cannot be lower than 9')
-        starting()
-    else:
-        print(f'Starting size: {n_size}')
-        return n_size
+    n_size = 0
+    while n_size < 9:
+        n_size = int(input('Starting population: '))
+        
+        if n_size >= 9:
+            print(f'Starting size: {n_size}')
+            return n_size
+        else:
+            print('Starting pop cannot be lower than 9')
+
+    
 
 def ending():
     global n
-    end = int(input('Ending population: '))
-    if end <= n:
-        print('Must be higher than starting pop')
-        ending()
-    else:
-        print(f'Ending size: {end}')
-        return end
+    end = 0
+    while end <= n:
+        end = int(input('Ending population: '))
+        
+        if end <= n:
+            print('Must be higher than starting pop')
+        else:
+            print(f'Ending size: {end}')
+            return end
+
 
 findpop()
 
